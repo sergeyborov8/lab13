770403f31a9b79ab99185aee9f4512dfe1472b61
serg <sergborovkov@gmail.com>
2017-11-27 21:44:36 +0300

    added sources

--- sources/pack.cpp
+++ sources/pack.cpp
@@ -1,50 +1,93 @@
-#include <string>
+#include "pugixml.hpp"
 #include <iostream>
-#include <sstream>
-#include <fstream>
-#include <vector>
-#include <nlohmann/json.hpp>
 
-using namespace std;
-using json = nlohmann::json;
 
 struct Email {
-  std::string nickname;
-  std::string server;
+    std::string nickname;
+    std::string server;
 };
 
 struct Person {
-  std::string  first_name;
-  std::string  last_name;
-  Email        email;
-  size_t       age;
-  std::string  phone;
+    std::string first_name;
+    std::string last_name;
+    Email email;
+    size_t age;
+    std::string phone;
 };
 
-Person getInfo() {
-	class Person file;
-  file.first_name = "Sergey";
-	file.last_name = "Borovkov";
-	file.email.nickname = "sergborovkov@gmail.com";
-	file.age = 23;
-	file.phone = "8(800)555-35-35";
-	return file;
+
+std::istream &operator>>(std::istream &is, Email &obj) {
+    std::string BUFF;
+
+    is >> BUFF;
+
+    size_t i = BUFF.find_first_of('@');
+
+    if (i >= 0) {
+        obj.nickname = BUFF.substr(0, i);
+        obj.server = BUFF.substr(i + 1, BUFF.size());
+    }
+
+    return is;
 }
 
+std::istream &operator>>(std::istream &is, Person &obj) {
+    std::cout << "Enter data to fields of the Person structure." << std::endl;
+    std::cout << "First name: ";
+    is >> obj.first_name;
+    std::cout << "Last name: ";
+    is >> obj.last_name;
+    std::cout << "Email: ";
+    is >> obj.email;
+    std::cout << "Age(optional): ";
+    std::string age;
+    std::cin.ignore();
+    std::getline(is, age);
+    if (!age.empty() && std::stoi(age) > 0)
+        obj.age = std::stoi(age);
+    else obj.age = 0;
+    std::cout << "Phone(optional): ";
+    std::getline(is, obj.phone);
+    return is;
+}
 
-int main(int argc, char* argv[]) {
-  string Path = argv[1];
-  Person Pers;
-  ofstream fout(Path);
+void SaveToFile(const Person &st, const char *path) {
+    pugi::xml_document doc;
 
-  Pers = getInfo();
-  json j;
-  j["first_name"] = Pers.first_name;
-  j["last_name"] = Pers.last_name;
-  j["email_nickname"] = Pers.email.nickname;
-  j["age"] = Pers.age;
-  j["phone"] = Pers.phone;
-  fout << j;
-  fout.close();
-  return 0;
+    pugi::xml_node node = doc.append_child("Person");
+
+    pugi::xml_node first_name = node.append_child("first_name");
+    first_name.append_child(pugi::node_pcdata).set_value(st.first_name.c_str());
+
+    pugi::xml_node last_name = node.append_child("last_name");
+    last_name.append_child(pugi::node_pcdata).set_value(st.last_name.c_str());
+
+    pugi::xml_node node2 = node.append_child("Email");
+
+    pugi::xml_node nickname = node2.append_child("nickname");
+    nickname.append_child(pugi::node_pcdata).set_value(st.email.nickname.c_str());
+
+    pugi::xml_node server = node2.append_child("server");
+    server.append_child(pugi::node_pcdata).set_value(st.email.server.c_str());
+
+    pugi::xml_node age = node.append_child("age");
+    age.append_child(pugi::node_pcdata).set_value((std::to_string(st.age)).c_str());
+
+    pugi::xml_node phone = node.append_child("phone");
+    phone.append_child(pugi::node_pcdata).set_value(st.phone.c_str());
+
+    std::cout << path;
+    doc.save_file(path);
+}
+
+int main(int argc, char *argv[]) {
+    char* path = argv[1];
+
+    Person pr;
+    std::cin >> pr;
+
+
+
+    SaveToFile(pr, path);
+    return 0;
 }
--- sources/unpack.cpp
+++ sources/unpack.cpp
@@ -1,48 +1,49 @@
-#include <string>
+#include "pugixml.hpp"
 #include <iostream>
-#include <sstream>
-#include <fstream>
-#include <vector>
-#include <nlohmann/json.hpp>
 
-using namespace std;
-using json = nlohmann::json;
 
 struct Email {
-  string nickname;
-  string server;
+    std::string nickname;
+    std::string server;
 };
 
 struct Person {
-  string  first_name;
-  string  last_name;
-  Email   email;
-  size_t  age;
-  string  phone;
+    std::string first_name;
+    std::string last_name;
+    Email email;
+    size_t age;
+    std::string phone;
 };
 
-int main(int argc, char* argv[]) {
-  string Path = argv[1];
-  struct Person Back;
-  json j;
-  ifstream f;
-  f.open(Path);
-  f >> j;
-  f.close();
-  Back.first_name = j.at("first_name").get<std::string>();
-  Back.last_name = j.at("last_name").get<std::string>();
-  Back.email.nickname = j.at("email_nickname").get<std::string>();
-  Back.age  = j.at("age").get<size_t>();
-  Back.phone  = j.at("phone").get<std::string>();
 
+void ReadFromFile( const char *path){
+    pugi::xml_document doc;
+    std::string str = "first_name";
+    pugi::xml_parse_result result = doc.load_file(path);
+    pugi::xml_node tool = doc.child("Person");
+    std::cout<<"Person"<<std::endl;
+    char *name = "first_name";
+    std::cout <<"Fisrt name: " <<tool.child_value(name) << std::endl;
+    char *family = "last_name";
+    std::cout << "Last name: "<<tool.child_value(family) << std::endl;
+    char *email = "Email";
+    char *server = "server";
+    char *nickname = "nickname";
+    std::cout<<"Email:"<< tool.child(email).child_value(nickname)<<"@"<< tool.child(email).child_value(server) << std::endl;;
 
+    char *age = "age";
+    if(tool.child_value(age)!=""){
+        std::cout <<"Age: " <<tool.child_value(age) << std::endl;
+    }
+    char *phone = "phone";
+    if(tool.child_value(phone)!=""){
+        std::cout <<"Phone: "<< tool.child_value(phone) << std::endl;
+    }
 
-  cout << "[Person]" << endl;
-  cout << "First name: " << Back.first_name << endl;
-  cout << "Last name: " << Back.last_name << endl;
-  cout << "Email_nickname: " << Back.email.nickname << endl;
-  cout << "Age: " << Back.age << endl;
-  cout << "Phone: " << Back.phone << endl;
+}
 
-  return 0;
+int main(int argc, char *argv[]) {
+    char* path = argv[1];
+    ReadFromFile(path);
+    return 0;
 }
